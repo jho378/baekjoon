56b05e8da05524bf48d9cb114511ac3bff563599
jino <jho378@naver.com>
2021-11-03 10:38:35 +0900

    11022

---
+++ 11022.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split("\n");
+
+const N = parseInt(input[0]);
+
+const data = [];
+for(let i=1; i<N+1; i++){
+    data.push(input[i].split(" "));
+}
+
+for(i=0; i<N; i++){
+    console.log(`Case #${i+1}: ${parseInt(data[i][0])} + ${parseInt(data[i][1])} = ${parseInt(data[i][0]) + parseInt(data[i][1])}`)
+}
