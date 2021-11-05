e2f04dae2a2b2b8f3dc8ddf810bc50057d047dbd
jino <jho378@naver.com>
2021-11-06 08:39:42 +0900

    2438

---
+++ 2438.js
@@ -0,0 +1,7 @@
+const fs =require('fs');
+const input = fs.readFileSync('/dev/stdin').toString();
+const N = parseInt(input);
+
+for(let i=0; i<N; i++){
+    console.log(`${"*".repeat(i+1) +" ".repeat(N-i-1)}`)
+}
