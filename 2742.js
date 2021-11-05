7a945998c5b90e642668233c7cfe77b3883fe926
jino <jho378@naver.com>
2021-11-06 08:56:10 +0900

    2742

--- 2742.js
+++ 2742.js
@@ -3,6 +3,6 @@ const input = fs.readFileSync('/dev/stdin').toString();
 const N = parseInt(input);
 let str = "";
 for(let i=N; i>0; i--){
-    str.concat(i+"\n");
+    str += i+"\n";
 }
-console.log(str.trim());
+console.log(str.trim())
