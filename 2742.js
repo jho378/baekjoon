53dbb4794847d16e2dcfeb9bf254f6f748696d44
jino <jho378@naver.com>
2021-11-06 08:51:43 +0900

    2742

---
+++ 2742.js
@@ -0,0 +1,8 @@
+const fs =require('fs');
+const input = fs.readFileSync('/dev/stdin').toString();
+const N = parseInt(input);
+let str = "";
+for(let i=N; i>0; i--){
+    str.concat(i+"\n");
+}
+console.log(str.trim());
