6959a57ba01b3458d2886b25ad8fe5620acf0571
jino <jho378@naver.com>
2021-11-07 08:40:22 +0900

    15552

--- 15552.js
+++ 15552.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
-const input = fs.readFileSync('a.txt').toString().trim().split("\n");
+const input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
 
 const N = parseInt(input[0]);
-
+let str ="";
 for(let i=1; i<N+1; i++){
-    console.log(parseInt(input[i].split(" ")[0]) + parseInt(input[i].split(" ")[1]));
+    str += parseInt(input[i].split(" ")[0]) + parseInt(input[i].split(" ")[1]) + "\n";
 }
+
+console.log(str);
