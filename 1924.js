5ddaac4a02fa38441a7b9ef812d7871960ca8718
jino <jho378@naver.com>
2021-11-11 10:49:52 +0900

    1924

---
+++ 1924.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split("\n");
+
+const m = parseInt(input[0]);
+const d = parseInt(input[1]);
+const arr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const day = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+let sum =0;
+if(m===1)   sum = d;
+else{
+    for(let i=0; i<m-1; i++){
+        sum+= arr[i]
+    }
+    sum += d;
+}
+const res = sum % 7;
+console.log(day[res]);
