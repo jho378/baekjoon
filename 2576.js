b5fb4c4281c2141c1f3b36f0815316d199170626
jino <jho378@naver.com>
2021-11-18 15:34:03 +0900

    2576

---
+++ 2576.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
+
+const data = [];
+
+for(i=0; i<7; i++){
+    if(parseInt(input[i])%2 === 1)   data.push(parseInt(input[i]));
+    else continue;
+}
+
+const sum = data.reduce((p,c) => p+c, 0);
+const min = data.reduce((p,c) => Math.min(p,c), 101);
+console.log(`${sum}\n${min}`);
