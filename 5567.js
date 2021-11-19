4f2429a618cf43c54a5b3e9317bb598a5c8a36e1
jino <jho378@naver.com>
2021-11-19 16:41:03 +0900

    5567

--- 5567.js
+++ 5567.js
@@ -1,23 +1,26 @@
 const fs = require('fs');
 const input = fs.readFileSync('a.txt').toString().trim().split('\n');
-
+const total = parseInt(input[0]);
 const N = parseInt(input[1]);
 const data =[];
 for(let i=2; i<N+2; i++)    data.push(input[i].split(' '));
-
 const friends = [];
 const friendsOfFriends = [];
 for(i=0; i<N; i++){
     const x = parseInt(data[i][0]);
     const y = parseInt(data[i][1]);
-
-    if(x === 1 && !(friends.includes(y))) friends.push(y);
+    if(x === 1 && friends.indexOf(y) === -1) friends.push(y);
+    else if(y ===1 && friends.indexOf(x) === -1)    friends.push(x);
 }
 
 for(i=0; i<N; i++){
     const x = parseInt(data[i][0]);
     const y = parseInt(data[i][1]);
-    if(friends.includes(x) && !(friends.includes(y)))    friendsOfFriends.push(y);
-    else if(friends.includes(y) && !(friends.includes(x))) friendsOfFriends.push(x);
+    if(friends.indexOf(x)!== -1 && friends.indexOf(y) === -1)    friendsOfFriends.push(y);
+    else if(friends.indexOf(y)!== -1 && friends.indexOf(x) === -1 ) friendsOfFriends.push(x);
 }
-console.log(friends.length + friendsOfFriends.filter(e => e!==1).length);
+let sum = 0 ;
+for(i=2; i<=total; i++){
+    if(friendsOfFriends.indexOf(i)!== -1)   sum += 1; 
+}
+console.log(friends.length + sum);
