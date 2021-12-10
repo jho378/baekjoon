8204c06ae079ecaa1960589fb5d2f1dd49e6b656
jino <jho378@naver.com>
2021-12-10 12:25:03 +0900

    2609

---
+++ 2609.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const input = fs.readFileSync('a.txt').toString().trim().split(' ');
+const A = parseInt(input[0]);
+const B = parseInt(input[1]);
+let min = Math.min(A, B);
+let max = Math.max(A, B);
+
+let cnt = 1;
+let tmp = min;
+let _tmp = max;
+
+while(tmp--){
+    if(max%tmp===0 && min%tmp===0){
+        console.log(tmp);
+        break;
+    }
+    else continue;
+}
+
+while(true){
+    _tmp = max * cnt; 
+    if(_tmp%min===0){
+        console.log(max*cnt);
+        break;
+    }
+    else cnt++;
+}
