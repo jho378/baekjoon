3535ca90922e7cf3f15cf61c423e475cd7d6bedf
jinho <jho378@naver.com>
2022-01-22 22:50:38 +0900

    2446

---
+++ 2446.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const input = fs.readFileSync('a.txt').toString().trim();
+const N = parseInt(input);
+
+for(let i=0; i<N; i++){
+    let str = "";
+    str+= " ".repeat(i);
+    str+= "*".repeat(2*N-i*2-1);
+    console.log(str)
+}
+for(let i=0; i<N-1; i++){
+    let str = "";
+    str+=" ".repeat(N-2-i);
+    str+="*".repeat(2*i+3);
+    console.log(str);
+}
