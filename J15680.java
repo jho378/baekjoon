2506148c82ea689efbfa773e066dae0b1639f690
김진호 <jho378@naver.com>
2022-03-23 15:50:39 +0900

    15680.java

---
+++ J15680.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class J15680{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
+        int i = scanner.nextInt(); 
+        if(i==0){
+            System.out.println("YONSEI");
+        }
+        else{
+            System.out.println("Leading the Way to the Future");
+        }
+    }
+}
