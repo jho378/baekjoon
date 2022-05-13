f61f988242fa28a7efead11e644c93b85ae0375c
jho378 <jho378@naver.com>
2022-05-13 10:12:27 +0900

    11055.js

---
+++ 11055.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const input = fs.readFileSync('a.txt').toString().trim().split('\n');
+
+const N = parseInt(input[0]);
+const data = input[1].split(' ').map(e => parseInt(e));
+const storage = new Array(N).fill(0);
+
+for(let i=N-1; i>=0; i--){
+    let sum = data[i];
+    for(let j=i+1; j<N; j++){
+        if (data[i] >= data[j]){
+            continue;
+        }
+        else if ( data[i] < data[j]){
+            const post = storage.slice(j);
+            const tmp = [];
+            for (let idx=0; idx<post.length; idx++){
+                if(data[i] < data[j+idx]){
+                    tmp.push(post[idx]);
+                }
+            }
+            const max = tmp.reduce((p,c) => Math.max(p,c), 0);
+            sum += max;
+            
+            break;
+        }
+    }
+    storage[i] = sum;
+}
+console.log(storage.reduce((p,c) => Math.max(p,c), -1));
